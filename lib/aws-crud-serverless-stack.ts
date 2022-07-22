35bf57b744cac2c2f4f20f7144a26e39a0e867f3
Lalit Ghimire <lalitghimire@gmail.com>
2022-07-22 09:54:23 +0300

    create table in database

--- lib/aws-crud-serverless-stack.ts
+++ lib/aws-crud-serverless-stack.ts
@@ -1,16 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class AwsCrudServerlessStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCrudServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // database setup
+        const taskTable = new dynamodb.Table(this, 'tasks', {
+            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+            tableName: 'tasks',
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+    }
 }
