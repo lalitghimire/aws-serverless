62ae47a8981dc25eb4c941721dcdfdf630b3d42f
Lalit Ghimire <lalitghimire@gmail.com>
2022-07-22 11:53:29 +0300

    add lambda api integration

--- lib/aws-crud-serverless-stack.ts
+++ lib/aws-crud-serverless-stack.ts
@@ -1,6 +1,9 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsCrudServerlessStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,5 +15,24 @@ export class AwsCrudServerlessStack extends Stack {
             tableName: 'tasks',
             removalPolicy: RemovalPolicy.DESTROY,
         });
+
+        // define an api gateway
+        const taskApi = new RestApi(this, 'tasksApi');
+
+        // lambda functions
+        const createTask = new NodejsFunction(this, 'createExercises', {
+            entry: join(__dirname, '..', 'functions', 'createTask.ts'),
+            handler: 'handler',
+        });
+
+        // dynamodb permission to the lambda functions
+        taskTable.grantWriteData(createTask);
+
+        // api-lambda integration
+        const createTaskIntegration = new LambdaIntegration(createTask);
+
+        // create routes
+        const taskResources = taskApi.root.addResource('tasks');
+        taskResources.addMethod('POST', createTaskIntegration);
     }
 }
